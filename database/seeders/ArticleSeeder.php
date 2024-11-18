1d4e28088d8dcd09fb2f083d5c5fe31d5824f683
Matrostan <matrosovdream@gmail.com>
2024-11-18 07:03:55 +0100

    Test

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -13,8 +13,6 @@ class ArticleSeeder extends Seeder
      */
     public function run(): void
     {
-        
-        Article::factory(30)->create();
-
+        //Article::factory(30)->create();
     }
 }
