747e3693c77e3d86f1143e6663eab43831bc5b40
Matrostan <matrosovdream@gmail.com>
2025-01-13 16:45:13 +0100

    Changes

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -13,7 +13,7 @@ class UserObserver
      */
     public function created(User $user): void
     {
-        UserHelper::sendUserCreatedEmail($user);
+        //UserHelper::sendUserCreatedEmail($user);
     }
 
     /**
