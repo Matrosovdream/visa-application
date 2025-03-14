be21c1abf4128ebe5b061f3de0246877fe4b9700
dotfiler <matrosovdream@gmail.com>
2025-03-14 09:16:58 +0100

    Account controller rewritten, added AccountActions

--- app/Actions/Web/AccountActions.php
+++ app/Actions/Web/AccountActions.php
@@ -6,27 +6,17 @@ use Illuminate\Support\Facades\Hash;
 
 class AccountActions {
 
-    public static function settingsUpdate($request) {
+    public function index() {
+        return array('title' => 'Dashboard');
+    }
 
-        $current_password = $request->current_password;
+    public function settings() {
+        return array('title' => 'Settings');
+    }
+
+    public function settingsUpdate($request) {
+
         $new_password = $request->new_password;
-        $new_password_confirmation = $request->new_password_confirmation;
-
-        // Validate
-        $request->validate([
-            'current_password' => 'current_password',
-            'new_password' => 'required|min:8|confirmed',
-            'new_password_confirmation' => 'required|min:8'
-        ]);
-
-        // Check current password
-        if( !Auth::attempt(['email' => Auth::user()->email, 'password' => $current_password]) ) {
-            return redirect()->back()->with('error', 'Current password is incorrect');
-        }
-
-        if( $new_password != $new_password_confirmation ) {
-            return redirect()->back()->with('error', 'New password and confirm password do not match');
-        }
 
         // Update user password
         $user = Auth::user();
--- app/Http/Controllers/User/AccountController.php
+++ app/Http/Controllers/User/AccountController.php
@@ -5,24 +5,60 @@ use App\Http\Controllers\Controller;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use App\Actions\Web\AccountActions;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
 
+    protected $accountActions;
+
+    public function __construct(AccountActions $accountActions)
+    {
+        $this->accountActions = $accountActions;
+    }
+
     public function index()
     {
-        $data = array('title' => 'Dashboard');
-        return view('web.account.index', $data);
+        return view(
+            'web.account.index',
+            $this->accountActions->index()
+        );
     }
+
     public function settings()
     {
-        $data = array('title' => 'My settings', 'articles' => Article::paginate(10));
-        return view('web.account.settings', $data);
+        return view(
+            'web.account.settings', 
+            $this->accountActions->settings()
+        );
     }
 
     public function settingsUpdate( Request $request )
     {
-        AccountActions::settingsUpdate($request);
+
+        // Validate
+        $request->validate([
+            'current_password' => 'current_password',
+            'new_password' => 'required|min:8|confirmed',
+            'new_password_confirmation' => 'required|min:8'
+        ]);
+
+        $current_password = $request->current_password;
+        $new_password = $request->new_password;
+        $new_password_confirmation = $request->new_password_confirmation;
+
+        // Check for passwords identity
+        if( $new_password != $new_password_confirmation ) {
+            return redirect()->back()->with('error', 'New password and confirm password do not match');
+        }
+
+        // Check current password
+        if( !Auth::attempt(['email' => Auth::user()->email, 'password' => $current_password]) ) {
+            return redirect()->back()->with('error', 'Current password is incorrect');
+        }
+
+        $this->accountActions->settingsUpdate($request);
+
         return redirect()->back()->with('success', 'Settings updated successfully');
     }
 
