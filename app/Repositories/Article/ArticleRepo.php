cd02a2b0df6e6d81f501b65cc2ac2e7deb6300d3
dotfiler <matrosovdream@gmail.com>
2025-03-16 13:05:00 +0100

    Article repo, injected ArticleGroupRepo and connected to map method

--- app/Repositories/Article/ArticleRepo.php
+++ app/Repositories/Article/ArticleRepo.php
@@ -5,19 +5,23 @@ use App\Repositories\AbstractRepo;
 use App\Repositories\User\UserRepo;
 use App\Models\Article;
 
+
 class ArticleRepo extends AbstractRepo
 {
 
     protected $model;
     protected $fields = [];
     protected $userRepo;
-    protected $withRelations = ['author'];
+    protected $articleGroupRepo;
+    protected $withRelations = ['groups'];
 
     public function __construct() {
 
         $this->model = new Article();
 
         $this->userRepo = new UserRepo();
+
+        $this->articleGroupRepo = new ArticleGroupRepo();
 
     }
 
@@ -35,6 +39,7 @@ class ArticleRepo extends AbstractRepo
             'short_description' => $item->short_description,
             'content' => $item->content,
             'author' => $this->userRepo->mapItem( $item->author ),
+            'groups' => $this->articleGroupRepo->mapItems( $item->groups ),
             'published' => $item->published,
             'Model' => $item
         ];
