559dd112c82144f5ee0a5d077d4a1d04804b0141
dotfiler <matrosovdream@gmail.com>
2025-03-26 10:51:00 +0100

    SeaportsSeeder, deep optimization using Model upsert

--- database/seeders/SeaportsSeeder.php
+++ database/seeders/SeaportsSeeder.php
@@ -2,52 +2,50 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Country;
-use App\Models\Airport;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\Country;
 
 class SeaportsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        // Get countries map [slug => [id, code]]
+        $country_ids = Country::all()->pluck('code', 'slug')->mapWithKeys(function ($code, $slug) use (&$country_ids) {
+            $id = Country::where('slug', $slug)->value('id');
+            return [$slug => ['id' => $id, 'code' => $code]];
+        })->toArray();
 
-        // Get all countries
-        $countries = Country::all();
-        foreach ($countries as $country) {
-            $country_ids[$country->slug] = [
-                'id' => $country->id,
-                'code' => $country->code,
+        // Load seaports JSON
+        $ports = json_decode(file_get_contents(database_path('references/seaports.json')), true);
+
+        $batchData = [];
+        foreach ($ports as $code => $port) {
+            $countrySlug = Str::slug($port['country']);
+
+            if (!isset($country_ids[$countrySlug])) {
+                continue; // Skip unknown country
+            }
+
+            $batchData[] = [
+                'entity' => 'seaport',
+                'identity' => $code,
+                'name' => $port['name'] . ' Seaport',
+                'municipality' => $port['city'],
+                'country_id' => $country_ids[$countrySlug]['id'],
+                'iso_country' => $country_ids[$countrySlug]['code'],
             ];
         }
 
-        // Retrieve the countries from the JSON file
-        $ports = json_decode(file_get_contents(database_path('references/seaports.json')), true);
-
-        // Insert the countries into the database
-        foreach ($ports as $code=>$port) {
-
-            $countrySlug = Str::slug( $port['country'] );
-            if( !isset( $country_ids[ $countrySlug ] ) ) {
-                continue;
-            }
-
-            // Update or create aiport by ref_id
-            Airport::updateOrCreate([
-                'identity' => $code,
-            ], [
-                'entity' => 'seaport',
-                'name' => $port['name'].' Seaport',
-                'municipality' => $port['city'],
-                'country_id' => $country_ids[ $countrySlug ]['id'],
-                'iso_country' => $country_ids[ $countrySlug ]['code'],
-                'identity' => $code,
-            ]);
+        // Insert in chunks with upsert
+        $chunkSize = 500;
+        foreach (array_chunk($batchData, $chunkSize) as $chunk) {
+            DB::table('arrival_points')->upsert(
+                $chunk,
+                ['identity'], // Unique key
+                ['name', 'municipality', 'country_id', 'iso_country', 'entity'] // Fields to update
+            );
         }
-
     }
 }
